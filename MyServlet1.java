01ac43fbf3aa888871b697f46cc7bc3ea5e1b725
Revathi0307 <jannavarapu.revathi7@gmail.com>
2017-03-25 18:54:00 -0400

    Assignment1-part3
    Create 2 HTML pages, one with GET method and one with POST method, with the action to the same Servlet. Handle both the requests using doGet and doPost methods.

--- MyServlet1.java
+++ MyServlet1.java
@@ -8,22 +8,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/MyServlet")
-public class MyServlet extends HttpServlet {
+@WebServlet("/MyServlet1")
+public class MyServlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    public MyServlet() {
+       
+    
+    public MyServlet1() {
+        super();
     }
 
-
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Hello " + request.getParameter("username"));
+		doPost(request, response);
 
 	}
 
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		response.getWriter().append("Hello " + request.getParameter("username"));
+
 	}
 
 }
